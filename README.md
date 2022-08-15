85e1ebd61aef398d7b1999da0b0d61abac487bdc
Ian Kersz <kerszamaral@gmail.com>
2022-08-15 18:08:57 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositorio do trabalhos Final de Estrutura de Dados da UFRGS 2022/1
+
+## Por Ian Kersz Amaral e Nathan Alonso Guimarães
---
+++ code/main.c
@@ -0,0 +1,20 @@
+/* Ian && Nathan- Cartão UFRGS: 
+15.08.22
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    int x;
+    scanf("%d", &x);
+    
+    return 0;
+}
