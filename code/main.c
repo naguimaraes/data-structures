de512c4a04a10029c15330a984fa43f7e799cd2b
Ian Kersz <kerszamaral@gmail.com>
2022-08-21 21:23:30 -0300

    Updated main and output file

--- code/main.c
+++ code/main.c
@@ -9,9 +9,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+ 
+#include "files.h" // Functions to write the files
+#include "types.h" // Structs and functions for structs
+#include "trees.h" // Shared function between tress
 
-#include "shared.h" // Funções compartilhadas para criar e fazer o output
-
+//! Funções das arvores
 #include "ABP.h" // Cria as funções para a arvore ABP
 #include "AVL.h" // Cria as funções para a arvore AVL
 #include "RN.h" // Cria as funções para a arvore RN
@@ -19,21 +22,66 @@
 
 int main(int argc, char *argv[])
 {
-    Outputs output; // Cria a struct para o output
+    Outputs output = initOutput(argc, argv); // Cria a struct para o output
 
-    outputStart(argc, argv, &output); // Inicia o output
+    TreeStats *statsABP = getTreeStats(&output, 1);                 // Cria a struct para o contador da arvore ABP
+    TreeStats *statsAVL = getTreeStats(&output, 2);                 // Cria a struct para o contador da arvore AVL
+    // TreeStats *statsRN = getTreeStats(&output, 3);               // Cria a struct para o contador da arvore RN
+    // TreeStats *statsSplay = getTreeStats(&output, 4);            // Cria a struct para o contador da arvore Splay
 
-    Alimento test;
-    Ingerido test2 = {" ", 100};
-    while (!getAlimento(&test, output.arg1Foods))
+    ABP *ABPtree = createTree(); // Cria a arvore ABP
+    AVL *AVLtree = createTree(); // Cria a arvore AVL
+    // RN *RNtree = createTree(); // Cria a arvore RN
+    // Splay *Splaytree = createTree(); // Cria a arvore Splay
+
+    Food food;
+    while (!getFoodFromFile(&food, getFilePointer(&output, 1)))
     {
-        output.totalCals += caloriasIngeridas(test, test2);
-        char *test3 = getOutputString(test, test2);
-        outputToFile(test3, output.arg3OutputFile);
-        free(test3);
+        insertABP(&ABPtree, food, statsABP); // Insere o alimento na arvore ABP
+        insertAVL(&AVLtree, food, statsAVL); // Insere o alimento na arvore AVL
+        // insertRN(&RNtree, food, statsRN); // Insere o alimento na arvore RN
+        // insertSplay(&Splaytree, food, statsSplay); // Insere o alimento na arvore Splay
     }
 
-    outputEnd(&output); // Finaliza o output
+    displayTree(ABPtree, 1); // Exibe a arvore ABP
+    printf("\n\n");
+    displayTree(AVLtree, 1); // Exibe a arvore AVL
+    printf("\n\n");
+    // displayTree(RNtree, 1); // Exibe a arvore RN
+    // displayTree(Splaytree, 1); // Exibe a arvore Splay
+
+    Food ingested; // Cria o alimento que será ingerido
+
+    while (!getFoodFromFile(&ingested, getFilePointer(&output, 2)))
+    {
+        ABP *nodeABP = searchTree(ABPtree, ingested, statsABP); // Busca o alimento na arvore ABP
+        AVL *nodeAVL = searchTree(AVLtree, ingested, statsAVL); // Busca o alimento na arvore AVL
+        // RN *nodeRN = searchTree(RNtree, ingested, statsRN); // Busca o alimento na arvore RN
+        // Splay *nodeSplay = searchTree(Splaytree, ingested, statsSplay); // Busca o alimento na arvore Splay
+
+        if (nodeABP != NULL && nodeAVL != NULL /*&& nodeRN != NULL && nodeSplay != NULL */)
+        {
+            Food temp = getInfo(nodeABP); // Pega o alimento da arvore ABP
+            
+            addCals(&output, calsIngested(temp, ingested)); // Adiciona os calorias do alimento ao total de calorias
+            
+            char *outputString = getOutputString(temp, ingested); // Pega a string para o output
+            outputStringToFile(outputString, getFilePointer(&output, 3)); // Escreve a string no arquivo de output
+            free(outputString); // Libera a string
+        }
+    }
+    
+    updateTreeCounter(statsABP, ABPtree); // Atualiza as estatisticas do ABP
+    updateTreeCounter(statsAVL, AVLtree); // Atualiza as estatisticas do AVL
+    // updateTreeCounter(statsRN, RNtree); // Atualiza as estatisticas do RN
+    // updateTreeCounter(statsSplay, Splaytree); // Atualiza as estatisticas do Splay
+
+    ABPtree = destroyTree(ABPtree); // Destroi a arvore ABP
+    AVLtree = destroyTree(AVLtree); // Destroi a arvore AVL
+    // RNtree = destroyTree(RNtree); // Destroi a arvore RN
+    // Splaytree = destroyTree(Splaytree); // Destroi a arvore Splay
+
+    deletOutput(&output); // Finaliza o output do arquivo de saida
 
     return 0;
 }
