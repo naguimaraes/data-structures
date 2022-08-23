ec1b4501f9f97f5456ada035e21c5895cbbc41df
Ian Kersz <kerszamaral@gmail.com>
2022-08-22 21:52:32 -0300

    Fixed crashing if file was not properly formatted

--- code/files.c
+++ code/files.c
@@ -18,11 +18,16 @@ int getFoodFromFile(Food *food, FILE *file)
         food = NULL;        // Inicializa o alimento como nulo
         return 1;           // Retorna 1 se chegar no final do arquivo
     }
-
+    if (strchr(temp, ';') == NULL)
+    {
+        food = NULL;        // Inicializa o alimento como nulo
+        return 1;           // Retorna 1 se chegar no final do arquivo
+    }
+    
     strtok(temp, ";");                                                                      // Remove o ; do nome do alimento
     strcpy(food->name, temp);                                                               // Copia o nome do alimento para o struct
     for (int i = 0; i < strlen(food->name); i++) food->name[i] = tolower(food->name[i]);    // Converte o nome do alimento para minusculo
-    food->calories = atoi(strtok(NULL, ";,"));                                     // Pega a quantidade de calorias por 100 gramas do alimento
+    food->calories = atoi(strtok(NULL, ";"));                                               // Pega a quantidade de calorias por 100 gramas do alimento
     free(temp);                                                                             // Libera a memoria alocada para o nome do alimento
 
     return 0; // Retorna 0 se nao houver erros
@@ -102,28 +107,28 @@ Outputs initOutput(int argc, char *argv[])
 void deletOutput(Outputs *output)
 {
     FILE *file = getFilePointer(output, 3); // Muda a nomeclatura do arquivo de saida
-    TreeStats ABP = *getTreeStats(output, 1); // Pega as estatisticas da arvore
-    TreeStats AVL = *getTreeStats(output, 2); // Pega as estatisticas da arvore
-    TreeStats RN = *getTreeStats(output, 3);  // Pega as estatisticas da arvore
-    TreeStats Splay = *getTreeStats(output, 4); // Pega as estatisticas da arvore
 
     fprintf(file, "\nTotal de %d calorias consumidas no dia.\n\n", getCals(output)); // Escreve o cabecalho do arquivo de saida
 
+    TreeStats ABP = *getTreeStats(output, 1); // Pega as estatisticas da arvore
     char *outputString = printStatistics(ABP); // Pega a string de saida do ABP
     fprintf(file, "%s\n", outputString);       // Escreve a string de saida no arquivo de saida
     free(outputString);                        // Libera a memoria alocada para a string de saida
 
+    TreeStats AVL = *getTreeStats(output, 2); // Pega as estatisticas da arvore
     outputString = printStatistics(AVL);   // Pega a string de saida do AVL
     fprintf(file, "%s\n", outputString);         // Escreve a string de saida no arquivo de saida
     free(outputString);                          // Libera a memoria alocada para a string de saida
 
-    outputString = printStatistics(RN); // Pega a string de saida do RN
-    fprintf(file, "%s\n", outputString);        // Escreve a string de saida no arquivo de saida
-    free(outputString);                         // Libera a memoria alocada para a string de saida
+    // TreeStats RN = *getTreeStats(output, 3);  // Pega as estatisticas da arvore
+    // outputString = printStatistics(RN); // Pega a string de saida do RN
+    // fprintf(file, "%s\n", outputString);        // Escreve a string de saida no arquivo de saida
+    // free(outputString);                         // Libera a memoria alocada para a string de saida
 
-    outputString = printStatistics(Splay); // Pega a string de saida do Splay
-    fprintf(file, "%s\n", outputString);           // Escreve a string de saida no arquivo de saida
-    free(outputString);                            // Libera a memoria alocada para a string de saida
+    // TreeStats Splay = *getTreeStats(output, 4); // Pega as estatisticas da arvore
+    // outputString = printStatistics(Splay); // Pega a string de saida do Splay
+    // fprintf(file, "%s\n", outputString);           // Escreve a string de saida no arquivo de saida
+    // free(outputString);                            // Libera a memoria alocada para a string de saida
 
     fclose(getFilePointer(output, 1));      // Fecha o arquivo de alimentos
     fclose(getFilePointer(output, 2));      // Fecha o arquivo de dias
